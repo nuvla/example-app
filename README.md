8d6674eb7e70efdf42905d42b6c9027a8ce08d2b
Charles Loomis <cal@sixsq.com>
2019-08-15 16:10:19 +0200

    add application definition

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# example-app
-example multi-component nuvla application
+# Example Application
+
+An example application that consists of two components: an nginx web
+server and a Jupyter notebook.
+
+## How to register this application in a Nuvla installation
+
+To register this container on your Nuvla on-premise installation,
+clone the sources from the
+[nuvla/example-app](https://github.com/nuvla/example-app) GitHub.
+
+Define the following environmental variables:
+
+ - `NUVLA_ENDPOINT`: The endpoint of the Nuvla server,
+   e.g. `https://nuvla.io`. 
+
+ - `NUVLA_USERNAME`: The username of the account to use.
+
+ - `NUVLA_PASSWORD`: The password of the account to use.
+ 
+Then run the following commands:
+
+```sh
+pip install nuvla-api
+python add-module.py
+```
+
+You should now see the module component in the App Store called *My
+first application*.
---
+++ add-module.py
@@ -0,0 +1,88 @@
+import os
+
+#
+# Add module to Nuvla server.
+#
+# The following environmental variables can/must be defined:
+#
+# NUVLA_ENDPOINT: endpoint of Nuvla server, defaults to localhost
+# NUVLA_USERNAME: username to access Nuvla
+# NUVLA_PASSWORD: password to access Nuvla
+#
+
+from nuvla.api import Api as nuvla_Api
+
+nuvla_api = nuvla_Api(os.environ['NUVLA_ENDPOINT'], insecure=True)
+
+nuvla_api.login_password(os.environ['NUVLA_USERNAME'], os.environ['NUVLA_PASSWORD'])
+
+description = """An application composed of two services for demonstration and
+learning purposes. The goal is to show how the application developer
+can use environmental variables, secrets, configuration files, urls,
+and output parameters. To make everything visible in this application
+definition, no custom images have been used."""
+
+with open('docker-compose.yml') as x: docker_compose = x.read()
+
+with open('index.html') as x: index_html = x.read()
+
+with open('my_config.txt') as x: my_config_txt = x.read()
+
+with open('nuvla-app-demo.ipynb') as x: nuvla_app_demo_ipynb = x.read()
+
+application = {
+    "author" : "sixsq",
+    "urls" : [
+        ["Nginx simple", "http://${hostname}:${nginx_simple.tcp.80}"],
+        ["Jupyter advanced", "http://${hostname}:${jupyter_advanced.tcp.8888}"]
+    ],
+    "docker-compose" : docker_compose,
+    "environmental-variables" : [
+        {
+            "name" : "MY_ENV",
+            "required" : False,
+            "value" : "my env value",
+            "description" : "application defined environmental variable"
+        }, {
+            "name" : "GIVE_ME_SOME_INPUT",
+            "required" : True,
+            "description" : "Required environmental variable example"
+        }
+    ],
+    "output-parameters" : [
+        {
+            "name" : "edit-me",
+            "description" : "parameter to be set"
+        }
+    ],
+    "files" : [
+        {
+            "file-name" : "jupyter_token.txt",
+            "file-content" : "token-from-secret"
+        }, {
+            "file-name" : "index.html",
+            "file-content" : index_html
+        }, {
+            "file-name" : "my_config.txt",
+            "file-content" : my_config_txt
+        }, {
+            "file-name" : "nuvla-app-demo.ipynb",
+            "file-content" : nuvla_app_demo_ipynb
+        }
+    ]
+}
+
+module = {"name": "My first application",
+          "description": description,
+          "logo-url": "/ui/images/noimage.png",
+          "subtype": "application",
+          
+          "path": "examples/my-first-application",
+          "parent-path": "examples",
+          
+          "data-accept-content-types": ["application/octet-stream"],
+          "content": application}
+
+module_response = nuvla_api.add('module', module)
+module_id = module_response.data['resource-id']
+print("module id: %s" % module_id)
